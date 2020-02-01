dd5dc4f1b779fac3f53825ec65e1f798e9479eee
gunamata <gunasekhar@outlook.com>
2020-01-31 20:21:35 -0800

    Added Readme

---
+++ README.md
@@ -0,0 +1,4 @@
+
+To run this app:
+
+* Rin 'python TonePredictor.py'
--- TonePredictorAPI.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-app = FastAPI()
-@app.get('/tone')
-def toneEstimator():
-    tones = ["humerous","ironic","cynical"];
-    return random.choice(tones)
